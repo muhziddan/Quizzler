a114b639d8e00add53cbcb19c861edfe37f859f1
muhammad ziddan hidayatullah <ziddan.mz@gmail.com>
2022-04-26 15:34:57 +0700

    add IBOutlet and IBAction, add updateUI function

--- Quizzler/ViewController.swift
+++ Quizzler/ViewController.swift
@@ -8,11 +8,38 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
+    @IBOutlet weak var trueButton: UIButton!
+    @IBOutlet weak var falseButton: UIButton!
+    
+    let quiz = [
+        "Cow is a Mammal",
+        "Chicken has four leg",
+        "4 modulo 2 is 0"
+    ]
+    
+    var questionNumber = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        updateUI()
     }
-
-
+    
+    
+    @IBAction func answerButtonPressed(_ sender: UIButton) {
+        print("questionNumber :\(questionNumber)")
+        if questionNumber < quiz.count - 1 {
+            questionNumber += 1
+            print("called")
+            updateUI()
+        }
+    }
+    
+    func updateUI() {
+        questionLabel.text = quiz[questionNumber]
+    }
 }
