53ff608b613a1bc2162f673abddc6eca65d3e38b
muhammad ziddan hidayatullah <ziddan.mz@gmail.com>
2022-04-26 14:25:24 +0700

    Initial Commit

---
+++ Quizzler/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Quizzler
+//
+//  Created by Muhammad Ziddan Hidayatullah on 26/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
